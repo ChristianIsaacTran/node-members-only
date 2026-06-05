bbd92b66029d420c56563144c1b54a2ad60caf56
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-06-04 21:29:52 -0500

    FIX: added autocomplete="off" to password fields and homepage links to signup page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,8 @@
 const db = require("../models/queries");
 
 async function renderIndexPage(req, res) {
+    console.log(req.user);
+
 
     // gets req.user from passport.authorize() on successful authentication, and from the local strategy defined in passportConfig.js
     res.render("index", {user: req.user});
